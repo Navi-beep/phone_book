72248b07e47e577a8721b7f9724bfb00102ea745
Navi-Beep <estawars30@gmail.com>
2022-08-16 20:47:23 -0500

    issue with index.html

--- app/routes.py
+++ app/routes.py
@@ -14,9 +14,11 @@ def regis():
     form = RegisterForm()
     if form.validate_on_submit():
         print('Form is validated. NICE')
-        first_name=form.first_name.data
-        last_name=form.last_name.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
         phone_number = form.phone_number.data
         address = form.address.data
+        new_contact = Contact(first_name=first_name, last_name=last_name, phone_number = phone_number, address=address)
+        print(f"{new_contact.first_name} has been added to address book.") 
     return render_template('regis.html', form=form)
 

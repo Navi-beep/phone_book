10985b1b05a944ed310e7ed226b79f3b362f4b43
Navi-Beep <estawars30@gmail.com>
2022-08-19 15:35:06 -0500

    fixed login

--- app/routes.py
+++ app/routes.py
@@ -42,12 +42,12 @@ def addcontact():
     return render_template('addcontact.html', form=form)
 
 
-@app.route('/Login', methods=["GET", "POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
-        password = form.username.data
+        password = form.password.data
         user = User.query.filter_by(username=username).first()
         if user is not None and user.check_password(password):
             login_user(user)

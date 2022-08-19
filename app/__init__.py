845f4d4ee5f6f491dd059f1f8b4a354cfbc4fe04
Navi-Beep <estawars30@gmail.com>
2022-08-19 14:47:06 -0500

    dingus

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 
@@ -10,6 +11,11 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+login.login_message = 'You must be logged in in orer to do that ya dingus'
+login.login_message_category = 'danger'
+
 
 
 from . import routes, models
